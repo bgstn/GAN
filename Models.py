62c875e3631ada658c30f72472685b3a3e6b2f47
abgstn <njuyanzhang@163.com>
2019-07-15 23:50:55 +0800

    change discriminator and generator to use keras function API for more flexiability

--- Models.py
+++ Models.py
@@ -14,15 +14,23 @@ class ModelBase:
 
 
 class Discriminator(ModelBase):
-    def __init__(self):
+    def __init__(self, num_pixels, num_classes):
         super().__init__()
         self.name = "discriminator"
+        self.num_pixels = num_pixels
+        self.num_classes = num_classes
 
     def get_models(self):
-        model = Sequential()
-        model.add(Dense(num_pixels, input_dim=num_pixels, kernel_initializer='normal', activation='relu'))
-        model.add(Dense(num_classes, kernel_initializer='normal', activation='softmax'))
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+        inpt1 = Input(shape=(self.num_pixels,))
+        dense1 = Dense(self.num_pixels,kernel_initializer='normal',activation='relu')(inpt1)
+
+        inpt2 = Input(shape=(self.num_pixels,))
+        dense2 = Dense(self.num_pixels,kernel_initializer='normal',activation='relu')(inpt2)
+
+        concat_layer = concatenate([dense1, dense2], axis=-1)
+        logits = Dense(self.num_classes, kernel_initializer='normal',activation='softmax')(concat_layer)
+        model = Model([inpt1, inpt2], logits)
+        model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
         return model
 
     def train(self):
@@ -35,7 +43,19 @@ class Generator(ModelBase):
         self.name = "generator"
 
     def get_models(self):
-        raise NotImplementedError
+        inpt1 = Input(shape=input_dimension)
+        l = inpt1
+        for hid in hidden_size:
+            l = Dense(units=hid, kernel_initializer='normal',activation='relu')(l)
+        model_d = clone_model(model=discriminator)
+        for layer in model_d.layers:
+            layer.trainable = False
+
+        inpt2 = Input(shape=(784,))
+        out = model_d([l, inpt2])
+        model = Model([inpt1, inpt2], out)
+        model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+        return model
 
     def train(self):
         raise NotImplementedError
